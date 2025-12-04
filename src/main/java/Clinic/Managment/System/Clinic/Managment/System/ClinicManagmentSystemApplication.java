c54438a05ece7cb65e5fb4fe5c6d88bc4276a3f9
jonacuke <jonacuke28@gmail.com>
2025-12-04 20:32:33 +0100

    Initial commit

---
+++ src/main/java/Clinic/Managment/System/Clinic/Managment/System/ClinicManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package Clinic.Managment.System.Clinic.Managment.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Clinic/Managment/System/Clinic/Managment/System/ClinicManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Clinic.Managment.System.Clinic.Managment.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
